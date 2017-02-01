7636e3c5ad746a9f0d34cfcc7a6153666d6b4467
kellyskarritt <skarritt@msu.edu>
2017-01-31 22:50:58 -0500

    fixed errors

--- script.js
+++ script.js
@@ -12,9 +12,7 @@ var updateText = function () {
   var describe = describeInput.value
   var email = emailInput.value
   var phone = phoneInput.value
-
-  preview.innerHTML =
-  '<h1>Hi, my name is ' + firstName + ' ' + lastName + '!</h1>' +
+  var text = '<h1>Hi, my name is ' + firstName + ' ' + lastName + '!</h1>' +
   '<p>' + describe + '</p>' +
   '<p>' +
     'If you are interested in a date, you can email me at ' +
@@ -22,16 +20,8 @@ var updateText = function () {
     'or give me a call at ' +
     '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>.' +
   '</p>'
-
-  generated.textContent =
-  '<h1>Hi, my name is ' + firstName + ' ' + lastName + '!</h1>' +
-  '<p>' + describe + '</p>' +
-  '<p>' +
-    'If you are interested in a date, you can email me at ' +
-    '<a href="mailto:' + email + '" target="_blank">' + email + '</a> ' +
-    'or give me a call at ' +
-    '<a href="tel:' + phone + '" target="_blank">' + phone + '</a>.' +
-  '</p>'
+  generated.textContent = text
+  preview.innerHTML = text
 }
 
 firstNameInput.addEventListener('input', updateText)
